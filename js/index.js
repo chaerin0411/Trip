88dbdccab9c08805ba312723b7bd4bf5bd0781e2
Eom <bonjenny1@gmail.com>
2021-11-04 20:13:33 +0900

    11/04-carousel-완료

--- js/index.js
+++ js/index.js
@@ -3,35 +3,34 @@
 
 $(function() {
     var cont = $('.container')
-    var carr = $('.carr')
     var btns = $('.button')
+    var prev = $('.prev')
+    var next = $('.next')
+    var currentTab = $(this).attr("id")
 
-    carr.click(function() {
-        var currentTab = getCurrentTab();
-        btns.removeClass("selected");
-        cont.removeClass("selected");
-        $(btns[currentTab]).addClass("selected");
-        $(".cimg:nth-child("+ currentTab +")").addClass("selected");
-        cont.css("transform", "translateX("+ -1004*(currentTab-1) +"px)")
-        cont.css("transitionDuration", "700ms");
-    })
-
-    btns.click(function () {
-        var currentTab = getCurrentTab();
-        btns.removeClass("selected");
-        cont.removeClass("selected");
-        $(this).addClass("selected");
-        $(".cimg:nth-child("+ currentTab +")").addClass("selected");
-        cont.css("transform", "translateX("+ -1004*(currentTab-1) +"px)")
-        cont.css("transitionDuration", "700ms");
-    })
-
-    function getCurrentTab() {
-        var btns = $('.button')
-        for(var i =0; i < btns.length; i++){
-            if($(btns[i]).hasClass("selected")){
-                return currentTab = $(btns[i]).attr("id");
+    prev.click(function() {
+        if(currentTab != 1) {
+            currentTab--;
+            btns.removeClass("selected");
+            $(btns[currentTab-1]).addClass("selected");
+            cont.css("transitionDuration", "700ms");
+            cont.css("transform", "translateX("+ -1004*(currentTab-1) +"px)")
         }
-    }
-    }
+    })
+    next.click(function() {
+        if(currentTab != 4) {
+            currentTab++;
+            btns.removeClass("selected");
+            $(btns[currentTab-1]).addClass("selected");
+            cont.css("transitionDuration", "700ms");
+            cont.css("transform", "translateX("+ -1004*(currentTab-1) +"px)")
+        }
+    })
+    btns.click(function () {
+        currentTab = $(this).attr("id");
+        btns.removeClass("selected");
+        $(this).addClass("selected");
+        cont.css("transitionDuration", "700ms");
+        cont.css("transform", "translateX("+ -1004*(currentTab-1) +"px)");
+    })
 });
