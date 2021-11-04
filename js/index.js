8a6fd5b53f4173571c519278d975b5542ac184bc
Eom <bonjenny1@gmail.com>
2021-11-04 18:38:18 +0900

    11/04-carousel-수정중

--- js/index.js
+++ js/index.js
@@ -1,35 +1,37 @@
-$(function () {
+/* ============== index.js ============== */
+/* index.html의 js파일 */
 
-    var ratio = 32.91;
-    var center = 2;
-    $('.buttons .button').click(function () {
-        $(".button").removeClass("active");
-        $(".slide-item").removeClass("active");
+$(function() {
+    var cont = $('.container')
+    var carr = $('.carr')
+    var btns = $('.button')
 
-        $(this).addClass("active");
+    carr.click(function() {
         var currentTab = getCurrentTab();
-        $(".slide-item:nth-child("+ currentTab +")").addClass("active");
-    
-        $('.slide').css("transform", "translateX("+ -ratio * (currentTab - center) +"%)")
-        $(".slide-item.active").click(function () {
-            var _name = $(this).find("input[name=movieName]").val();
-            location.href = _name + ".html";
-        })
+        btns.removeClass("selected");
+        cont.removeClass("selected");
+        $(btns[currentTab]).addClass("selected");
+        $(".cimg:nth-child("+ currentTab +")").addClass("selected");
+        cont.css("transform", "translateX("+ -1004*(currentTab-1) +"px)")
+        cont.css("transitionDuration", "700ms");
     })
 
-    $(".slide-item.active").click(function () {
-        var _name = $(this).find("input[name=movieName]").val();
-        location.href = _name + ".html";
+    btns.click(function () {
+        var currentTab = getCurrentTab();
+        btns.removeClass("selected");
+        cont.removeClass("selected");
+        $(this).addClass("selected");
+        $(".cimg:nth-child("+ currentTab +")").addClass("selected");
+        cont.css("transform", "translateX("+ -1004*(currentTab-1) +"px)")
+        cont.css("transitionDuration", "700ms");
     })
-})
 
-function getCurrentTab() {
-    var buttons = $('.buttons div')
-    for(var i =0; i < buttons.length; i++){
-        if($(buttons[i]).hasClass("active")){
-            var currentTab = $(buttons[i]).attr("id").substr(1);
-            return currentTab;            
+    function getCurrentTab() {
+        var btns = $('.button')
+        for(var i =0; i < btns.length; i++){
+            if($(btns[i]).hasClass("selected")){
+                return currentTab = $(btns[i]).attr("id");
         }
     }
-    
-}
+    }
+});
